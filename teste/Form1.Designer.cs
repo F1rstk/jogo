b95226a57110c0fd16342f340080d9fb2231ee36
Filipe Santos Diniz <filipediniz465@gmail.com>
2025-01-27 17:05:45 -0300

    teste 0.0.2

--- teste/Form1.Designer.cs
+++ teste/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             txtNomeUsuario = new TextBox();
             btnAdicionarUsuario = new Button();
             txtPontuacao = new TextBox();
@@ -37,25 +38,26 @@
             btnExcluirUsuario = new Button();
             txtIdUsuario = new TextBox();
             lblid = new Label();
-            lblPergunta = new Label();
             btnResposta1 = new Button();
             btnResposta2 = new Button();
             btnResposta3 = new Button();
             btnResposta4 = new Button();
             btnResposta = new Button();
             btnIniciarJogo = new Button();
+            btnSair = new Button();
+            btnPergunta = new Button();
             SuspendLayout();
             // 
             // txtNomeUsuario
             // 
-            txtNomeUsuario.Location = new Point(32, 36);
+            txtNomeUsuario.Location = new Point(553, 168);
             txtNomeUsuario.Name = "txtNomeUsuario";
             txtNomeUsuario.Size = new Size(114, 23);
             txtNomeUsuario.TabIndex = 0;
             // 
             // btnAdicionarUsuario
             // 
-            btnAdicionarUsuario.Location = new Point(32, 76);
+            btnAdicionarUsuario.Location = new Point(553, 197);
             btnAdicionarUsuario.Name = "btnAdicionarUsuario";
             btnAdicionarUsuario.Size = new Size(75, 23);
             btnAdicionarUsuario.TabIndex = 1;
@@ -65,14 +67,14 @@
             // 
             // txtPontuacao
             // 
-            txtPontuacao.Location = new Point(228, 36);
+            txtPontuacao.Location = new Point(673, 168);
             txtPontuacao.Name = "txtPontuacao";
             txtPontuacao.Size = new Size(100, 23);
             txtPontuacao.TabIndex = 2;
             // 
             // btnCarregarUsuarios
             // 
-            btnCarregarUsuarios.Location = new Point(228, 76);
+            btnCarregarUsuarios.Location = new Point(634, 197);
             btnCarregarUsuarios.Name = "btnCarregarUsuarios";
             btnCarregarUsuarios.Size = new Size(75, 23);
             btnCarregarUsuarios.TabIndex = 3;
@@ -83,7 +85,7 @@
             // lblnome
             // 
             lblnome.AutoSize = true;
-            lblnome.Location = new Point(32, 9);
+            lblnome.Location = new Point(553, 150);
             lblnome.Name = "lblnome";
             lblnome.Size = new Size(40, 15);
             lblnome.TabIndex = 4;
@@ -92,7 +94,7 @@
             // lblpontuacao
             // 
             lblpontuacao.AutoSize = true;
-            lblpontuacao.Location = new Point(228, 9);
+            lblpontuacao.Location = new Point(673, 150);
             lblpontuacao.Name = "lblpontuacao";
             lblpontuacao.Size = new Size(64, 15);
             lblpontuacao.TabIndex = 5;
@@ -100,7 +102,7 @@
             // 
             // btnExcluirUsuario
             // 
-            btnExcluirUsuario.Location = new Point(395, 76);
+            btnExcluirUsuario.Location = new Point(713, 197);
             btnExcluirUsuario.Name = "btnExcluirUsuario";
             btnExcluirUsuario.Size = new Size(75, 23);
             btnExcluirUsuario.TabIndex = 6;
@@ -110,7 +112,7 @@
             // 
             // txtIdUsuario
             // 
-            txtIdUsuario.Location = new Point(395, 36);
+            txtIdUsuario.Location = new Point(629, 274);
             txtIdUsuario.Name = "txtIdUsuario";
             txtIdUsuario.Size = new Size(100, 23);
             txtIdUsuario.TabIndex = 7;
@@ -118,64 +120,67 @@
             // lblid
             // 
             lblid.AutoSize = true;
-            lblid.Location = new Point(395, 9);
+            lblid.Location = new Point(629, 244);
             lblid.Name = "lblid";
             lblid.Size = new Size(17, 15);
             lblid.TabIndex = 8;
             lblid.Text = "Id";
             // 
-            // lblPergunta
-            // 
-            lblPergunta.AutoSize = true;
-            lblPergunta.Location = new Point(142, 159);
-            lblPergunta.Name = "lblPergunta";
-            lblPergunta.Size = new Size(55, 15);
-            lblPergunta.TabIndex = 9;
-            lblPergunta.Text = "Pergunta";
-            // 
             // btnResposta1
             // 
-            btnResposta1.Location = new Point(62, 214);
+            btnResposta1.BackColor = Color.Transparent;
+            btnResposta1.FlatAppearance.BorderColor = Color.FromArgb(255, 223, 0);
+            btnResposta1.FlatStyle = FlatStyle.Flat;
+            btnResposta1.Location = new Point(50, 168);
             btnResposta1.Name = "btnResposta1";
-            btnResposta1.Size = new Size(153, 76);
+            btnResposta1.Size = new Size(324, 35);
             btnResposta1.TabIndex = 10;
             btnResposta1.Text = "button1";
-            btnResposta1.UseVisualStyleBackColor = true;
+            btnResposta1.UseVisualStyleBackColor = false;
             btnResposta1.Click += btnResposta1_Click;
             // 
             // btnResposta2
             // 
-            btnResposta2.Location = new Point(323, 214);
+            btnResposta2.BackColor = Color.Transparent;
+            btnResposta2.FlatAppearance.BorderColor = Color.FromArgb(255, 223, 0);
+            btnResposta2.FlatStyle = FlatStyle.Flat;
+            btnResposta2.Location = new Point(50, 227);
             btnResposta2.Name = "btnResposta2";
-            btnResposta2.Size = new Size(132, 76);
+            btnResposta2.Size = new Size(324, 32);
             btnResposta2.TabIndex = 11;
             btnResposta2.Text = "button1";
-            btnResposta2.UseVisualStyleBackColor = true;
+            btnResposta2.UseVisualStyleBackColor = false;
             btnResposta2.Click += btnResposta2_Click;
             // 
             // btnResposta3
             // 
-            btnResposta3.Location = new Point(62, 296);
+            btnResposta3.BackColor = Color.Transparent;
+            btnResposta3.FlatAppearance.BorderColor = Color.FromArgb(255, 223, 0);
+            btnResposta3.FlatStyle = FlatStyle.Flat;
+            btnResposta3.Location = new Point(50, 274);
             btnResposta3.Name = "btnResposta3";
-            btnResposta3.Size = new Size(153, 94);
+            btnResposta3.Size = new Size(324, 39);
             btnResposta3.TabIndex = 12;
             btnResposta3.Text = "button1";
-            btnResposta3.UseVisualStyleBackColor = true;
+            btnResposta3.UseVisualStyleBackColor = false;
             btnResposta3.Click += btnResposta3_Click;
             // 
             // btnResposta4
             // 
-            btnResposta4.Location = new Point(323, 296);
+            btnResposta4.BackColor = Color.Transparent;
+            btnResposta4.FlatAppearance.BorderColor = Color.FromArgb(255, 223, 0);
+            btnResposta4.FlatStyle = FlatStyle.Flat;
+            btnResposta4.Location = new Point(50, 329);
             btnResposta4.Name = "btnResposta4";
-            btnResposta4.Size = new Size(132, 94);
+            btnResposta4.Size = new Size(324, 34);
             btnResposta4.TabIndex = 13;
             btnResposta4.Text = "button1";
-            btnResposta4.UseVisualStyleBackColor = true;
+            btnResposta4.UseVisualStyleBackColor = false;
             btnResposta4.Click += btnResposta4_Click;
             // 
             // btnResposta
             // 
-            btnResposta.Location = new Point(492, 296);
+            btnResposta.Location = new Point(629, 367);
             btnResposta.Name = "btnResposta";
             btnResposta.Size = new Size(75, 23);
             btnResposta.TabIndex = 14;
@@ -185,7 +190,7 @@
             // 
             // btnIniciarJogo
             // 
-            btnIniciarJogo.Location = new Point(492, 251);
+            btnIniciarJogo.Location = new Point(629, 320);
             btnIniciarJogo.Name = "btnIniciarJogo";
             btnIniciarJogo.Size = new Size(75, 23);
             btnIniciarJogo.TabIndex = 15;
@@ -193,18 +198,45 @@
             btnIniciarJogo.UseVisualStyleBackColor = true;
             btnIniciarJogo.Click += btnIniciarJogo_Click_1;
             // 
+            // btnSair
+            // 
+            btnSair.Location = new Point(629, 406);
+            btnSair.Name = "btnSair";
+            btnSair.Size = new Size(75, 23);
+            btnSair.TabIndex = 16;
+            btnSair.Text = "&Sair";
+            btnSair.UseVisualStyleBackColor = true;
+            btnSair.Click += btnSair_Click;
+            // 
+            // btnPergunta
+            // 
+            btnPergunta.BackColor = Color.Transparent;
+            btnPergunta.FlatAppearance.BorderColor = Color.FromArgb(255, 223, 0);
+            btnPergunta.FlatStyle = FlatStyle.Flat;
+            btnPergunta.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnPergunta.Location = new Point(50, 48);
+            btnPergunta.Name = "btnPergunta";
+            btnPergunta.Size = new Size(453, 87);
+            btnPergunta.TabIndex = 17;
+            btnPergunta.Text = "Pergunta";
+            btnPergunta.TextAlign = ContentAlignment.TopLeft;
+            btnPergunta.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnPergunta);
+            Controls.Add(btnSair);
             Controls.Add(btnIniciarJogo);
             Controls.Add(btnResposta);
             Controls.Add(btnResposta4);
             Controls.Add(btnResposta3);
             Controls.Add(btnResposta2);
             Controls.Add(btnResposta1);
-            Controls.Add(lblPergunta);
             Controls.Add(lblid);
             Controls.Add(txtIdUsuario);
             Controls.Add(btnExcluirUsuario);
@@ -214,6 +246,7 @@
             Controls.Add(txtPontuacao);
             Controls.Add(btnAdicionarUsuario);
             Controls.Add(txtNomeUsuario);
+            DoubleBuffered = true;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -232,12 +265,13 @@
         private Button btnExcluirUsuario;
         private TextBox txtIdUsuario;
         private Label lblid;
-        private Label lblPergunta;
         private Button btnResposta1;
         private Button btnResposta2;
         private Button btnResposta3;
         private Button btnResposta4;
         private Button btnResposta;
         private Button btnIniciarJogo;
+        private Button btnSair;
+        private Button btnPergunta;
     }
 }
--- teste/Form1.cs
+++ teste/Form1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 namespace teste
@@ -26,71 +27,118 @@ namespace teste
 
         private MySqlConnection ConectarBD()
         {
-            MySqlConnection conn = new MySqlConnection(connectionString);
+            MySqlConnection conn = null;
             try
             {
+                conn = new MySqlConnection(connectionString);
                 conn.Open();
-                MessageBox.Show("Conexão bem-sucedida!");
+                return conn;
+            }
+            catch (MySqlException ex)
+            {
+                // Aqui você pode tratar erros específicos do MySQL, como problemas de conexão.
+                MostrarErro("Erro na conexão com o banco de dados.", ex);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Erro na conexão: " + ex.Message);
+                // Trata exceções gerais.
+                MostrarErro("Erro inesperado ao conectar com o banco de dados.", ex);
             }
-            return conn;
+            return null;
         }
+
+        private void MostrarErro(string mensagem, Exception ex)
+        {
+            // Exibe uma mensagem para o usuário e loga o erro (log pode ser feito em um arquivo ou banco de dados)
+            MessageBox.Show($"{mensagem}\nDetalhes: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            // Opcionalmente, logue o erro em um arquivo de log ou banco de dados:
+            // LogError(ex);
+        }
+
+
 
         private void CarregarUsuarios()
         {
             try
             {
-                MySqlConnection conn = ConectarBD();
-                string query = "SELECT * FROM Usuario";
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                MySqlDataReader reader = cmd.ExecuteReader();
-
-                while (reader.Read())
+                using (MySqlConnection conn = ConectarBD())
                 {
-                    // Exemplo de como exibir os dados de cada usuário
-                    Console.WriteLine($"ID: {reader["idUsuario"]}, Nome: {reader["nomeUsuario"]}, Pontuação: {reader["quantidadePontuacao"]}");
-                }
+                    if (conn == null)
+                        return;
 
-                conn.Close();
+                    string query = "SELECT * FROM Usuario";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Exemplo de como exibir os dados de cada usuário
+                            Console.WriteLine($"ID: {reader["idUsuario"]}, Nome: {reader["nomeUsuario"]}, Pontuação: {reader["quantidadePontuacao"]}");
+                        }
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MostrarErro("Erro ao carregar os usuários do banco de dados.", ex);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Erro ao carregar usuários: " + ex.Message);
+                MostrarErro("Erro inesperado ao carregar os usuários.", ex);
             }
         }
+
 
 
         private void AdicionarUsuario(string nomeUsuario, int quantidadePontuacao)
         {
             try
             {
-                MySqlConnection conn = ConectarBD();
-                string query = "INSERT INTO Usuario (nomeUsuario, quantidadePontuacao) VALUES (@nomeUsuario, @quantidadePontuacao)";
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("@nomeUsuario", nomeUsuario);
-                cmd.Parameters.AddWithValue("@quantidadePontuacao", quantidadePontuacao);
+                using (MySqlConnection conn = ConectarBD())
+                {
+                    if (conn == null)
+                        return;
 
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Usuário adicionado com sucesso!");
+                    string query = "INSERT INTO Usuario (nomeUsuario, quantidadePontuacao) VALUES (@nomeUsuario, @quantidadePontuacao)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@nomeUsuario", nomeUsuario);
+                        cmd.Parameters.AddWithValue("@quantidadePontuacao", quantidadePontuacao);
 
-                conn.Close();
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Usuário adicionado com sucesso!");
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MostrarErro("Erro ao adicionar o usuário no banco de dados.", ex);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Erro ao adicionar usuário: " + ex.Message);
+                MostrarErro("Erro inesperado ao adicionar o usuário.", ex);
             }
-
         }
+
 
         private void btnAdicionarUsuario_Click(object sender, EventArgs e)
         {
-            string nomeUsuario = txtNomeUsuario.Text;
-            int quantidadePontuacao = int.Parse(txtPontuacao.Text);
+            string nomeUsuario = txtNomeUsuario.Text.Trim();
+            if (string.IsNullOrEmpty(nomeUsuario))
+            {
+                MessageBox.Show("O nome do usuário não pode ser vazio.");
+                return;
+            }
+
+            if (!int.TryParse(txtPontuacao.Text, out int quantidadePontuacao))
+            {
+                MessageBox.Show("A pontuação deve ser um número válido.");
+                return;
+            }
+
             AdicionarUsuario(nomeUsuario, quantidadePontuacao);
         }
+
 
         private void btnCarregarUsuarios_Click(object sender, EventArgs e)
         {
@@ -101,29 +149,38 @@ namespace teste
         {
             try
             {
-                MySqlConnection conn = ConectarBD();
-                string query = "DELETE FROM Usuario WHERE idUsuario = @idUsuario";
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("@idUsuario", idUsuario); // Passando o id do usuário a ser excluído
-
-                int rowsAffected = cmd.ExecuteNonQuery();
-
-                if (rowsAffected > 0)
+                using (MySqlConnection conn = ConectarBD())
                 {
-                    MessageBox.Show("Usuário excluído com sucesso!");
-                }
-                else
-                {
-                    MessageBox.Show("Nenhum usuário encontrado com o ID fornecido.");
-                }
+                    if (conn == null)
+                        return;
 
-                conn.Close();
+                    string query = "DELETE FROM Usuario WHERE idUsuario = @idUsuario";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@idUsuario", idUsuario);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Usuário excluído com sucesso!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Nenhum usuário encontrado com o ID fornecido.");
+                        }
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MostrarErro("Erro ao excluir o usuário no banco de dados.", ex);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Erro ao excluir usuário: " + ex.Message);
+                MostrarErro("Erro inesperado ao excluir o usuário.", ex);
             }
         }
+
 
         private void btnExcluirUsuario_Click(object sender, EventArgs e)
         {
@@ -150,50 +207,84 @@ namespace teste
 
         private void CarregarPergunta()
         {
-            MySqlConnection conn = ConectarBD();
-            string query = "SELECT * FROM Pergunta WHERE jaUtilizada = 0 ORDER BY RAND() LIMIT 1";
-            MySqlCommand cmd = new MySqlCommand(query, conn);
-            MySqlDataReader reader = cmd.ExecuteReader();
-
-            if (reader.Read())
+            try
             {
-                string pergunta = reader["pergunta"].ToString();
-                string resposta1 = reader["resposta1"].ToString();
-                string resposta2 = reader["resposta2"].ToString();
-                string resposta3 = reader["resposta3"].ToString();
-                string resposta4 = reader["resposta4"].ToString();
-                int idPergunta = Convert.ToInt32(reader["idPergunta"]);
-                int respostaCorreta = Convert.ToInt32(reader["respostaCorreta"]);
-                int pontosPergunta = Convert.ToInt32(reader["pontosPergunta"]);
+                using (MySqlConnection conn = ConectarBD())
+                {
+                    if (conn == null)
+                        return;
 
-                perguntasUsadas.Add(idPergunta);
-                AtualizarPergunta(pergunta, resposta1, resposta2, resposta3, resposta4, respostaCorreta, pontosPergunta);
+                    string query = "SELECT * FROM Pergunta WHERE jaUtilizada = 0 ORDER BY RAND() LIMIT 1";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string pergunta = reader["pergunta"].ToString();
+                            string resposta1 = reader["resposta1"].ToString();
+                            string resposta2 = reader["resposta2"].ToString();
+                            string resposta3 = reader["resposta3"].ToString();
+                            string resposta4 = reader["resposta4"].ToString();
+                            int idPergunta = Convert.ToInt32(reader["idPergunta"]);
+                            int respostaCorreta = Convert.ToInt32(reader["respostaCorreta"]);
+                            int pontosPergunta = Convert.ToInt32(reader["pontosPergunta"]);
 
-                // Marca a pergunta como utilizada
-                string updateQuery = "UPDATE Pergunta SET jaUtilizada = 1 WHERE idPergunta = @idPergunta";
-                MySqlCommand updateCmd = new MySqlCommand(updateQuery, conn);
-                updateCmd.Parameters.AddWithValue("@idPergunta", idPergunta);
-                updateCmd.ExecuteNonQuery();
+                            perguntasUsadas.Add(idPergunta);
+                            AtualizarPergunta(pergunta, resposta1, resposta2, resposta3, resposta4, respostaCorreta, pontosPergunta);
+
+                            // Agora podemos fazer uma segunda consulta em outra conexão, se necessário
+                            CarregarOutraPergunta();  // Exemplo de outra consulta
+                        }
+                    }
+                }
             }
-
-            conn.Close();
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar pergunta: " + ex.Message);
+            }
         }
+
+        private void CarregarOutraPergunta()
+        {
+            try
+            {
+                using (MySqlConnection conn2 = ConectarBD())
+                {
+                    if (conn2 == null)
+                        return;
+
+                    string query = "SELECT * FROM Pergunta";
+                    using (MySqlCommand cmd2 = new MySqlCommand(query, conn2))
+                    using (MySqlDataReader reader2 = cmd2.ExecuteReader())
+                    {
+                        // Faz algo com os dados de outra consulta
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar outra pergunta: " + ex.Message);
+            }
+        }
+
+
 
 
         private void AtualizarPergunta(string pergunta, string resposta1, string resposta2, string resposta3, string resposta4, int respostaCorreta, int pontosPergunta)
         {
-            lblPergunta.Text = pergunta;
+            btnPergunta.Text = pergunta;
             btnResposta1.Text = resposta1;
             btnResposta2.Text = resposta2;
             btnResposta3.Text = resposta3;
             btnResposta4.Text = resposta4;
 
-            // Armazena a resposta correta
-            btnResposta1.Tag = (respostaCorreta == 1) ? pontosPergunta : 10;
-            btnResposta2.Tag = (respostaCorreta == 2) ? pontosPergunta : 10;
-            btnResposta3.Tag = (respostaCorreta == 3) ? pontosPergunta : 10;
-            btnResposta4.Tag = (respostaCorreta == 4) ? pontosPergunta : 10;
+            // Armazenamos a resposta correta no Tag de cada botão
+            btnResposta1.Tag = (respostaCorreta == 1) ? pontosPergunta : 0;
+            btnResposta2.Tag = (respostaCorreta == 2) ? pontosPergunta : 0;
+            btnResposta3.Tag = (respostaCorreta == 3) ? pontosPergunta : 0;
+            btnResposta4.Tag = (respostaCorreta == 4) ? pontosPergunta : 0;
         }
+
 
         private void VerificarResposta(Button respostaSelecionada)
         {
@@ -209,12 +300,13 @@ namespace teste
                 MessageBox.Show($"Você errou! Pontuação atual: {pontuacao}");
             }
 
-            // Atualiza a pontuação do usuário no banco de dados
+            // Atualiza a pontuação do usuário no banco
             AtualizarPontuacaoUsuario();
 
             // Carrega a próxima pergunta
             CarregarPergunta();
         }
+
 
         private void AtualizarPontuacaoUsuario()
         {
@@ -265,6 +357,11 @@ namespace teste
         private void btnResposta4_Click(object sender, EventArgs e)
         {
             VerificarResposta(btnResposta4);
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
